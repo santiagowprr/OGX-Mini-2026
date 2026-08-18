5b87b791bd043d0e3b4303c6c1b81c230b1fa6ba
Jakub <62140858+santiagowprr@users.noreply.github.com>
2026-08-18 21:57:09 +0200

    Refactor init_git_submodules function for simplicity

--- Firmware/cmake/init_submodules.cmake
+++ Firmware/cmake/init_submodules.cmake
@@ -1,20 +1,18 @@
-function(init_git_submodules EXTERNAL_DIR SUBMODULE_PATHS)
-    set(REPO_DIR "${EXTERNAL_DIR}/../../")
+function(init_git_submodules EXTERNAL_DIR)
+    get_filename_component(REPO_DIR "${EXTERNAL_DIR}/../.." ABSOLUTE)
     message(STATUS "Initializing submodules in ${REPO_DIR}")
 
-    foreach(SUBMODULE_PATH ${SUBMODULE_PATHS})
-        execute_process(
-            COMMAND git submodule update --init --recursive ${SUBMODULE_PATH}
-            WORKING_DIRECTORY ${REPO_DIR}
-            RESULT_VARIABLE INIT_SUBMODULE_RESULT
-            OUTPUT_VARIABLE INIT_SUBMODULE_OUTPUT
-            ERROR_VARIABLE INIT_SUBMODULE_ERROR
-        )
+    execute_process(
+        COMMAND git submodule update --init --recursive
+        WORKING_DIRECTORY ${REPO_DIR}
+        RESULT_VARIABLE INIT_SUBMODULE_RESULT
+        OUTPUT_VARIABLE INIT_SUBMODULE_OUTPUT
+        ERROR_VARIABLE INIT_SUBMODULE_ERROR
+    )
 
-        if(INIT_SUBMODULE_RESULT EQUAL 0)
-            message(STATUS "Submodules initialized successfully.")
-        else()
-            message(FATAL_ERROR "Failed to initialize submodules: ${INIT_SUBMODULE_ERROR}")
-        endif()
-    endforeach()
+    if(INIT_SUBMODULE_RESULT EQUAL 0)
+        message(STATUS "Submodules initialized successfully.")
+    else()
+        message(FATAL_ERROR "Failed to initialize submodules: ${INIT_SUBMODULE_ERROR}")
+    endif()
 endfunction()
